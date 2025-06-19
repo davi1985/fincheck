0244cbaa69f4804f7ea85798ad4f2dd76d11b94c
Davi Silva <davisilvaphoto@gmail.com>
2025-06-19 10:03:33 -0300

    feat: create user module

---
+++ api/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prismaService.user.create({
+      data: {
+        email: createUserDto.email,
+        name: createUserDto.name,
+        password: createUserDto.password,
+      },
+    });
+
+    return user;
+  }
+}
