134cfd08898af5083ab88eaad2fb9a0c13fcf11d
Davi Silva <davisilvaphoto@gmail.com>
2025-06-21 17:57:47 -0300

    organize imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,19 @@ import { APP_GUARD } from '@nestjs/core';
 
 import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { UsersModule } from './modules/users';
 import { DatabaseModule } from './shared/database/database.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+    CategoriesModule,
+    BankAccountsModule,
+  ],
   controllers: [],
   providers: [
     {
