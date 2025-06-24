647d93ec9705a685180419dc9b836b5340e8aeb2
Davi Silva <davisilvaphoto@gmail.com>
2025-06-23 21:09:06 -0300

    remove unused file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,9 @@ import { AuthGuard } from './modules/auth/auth.guard';
 import { AuthModule } from './modules/auth/auth.module';
 import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 import { CategoriesModule } from './modules/categories/categories.module';
-import { UsersModule } from './modules/users';
-import { DatabaseModule } from './shared/database/database.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
+import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './shared/database/database.module';
 
 @Module({
   imports: [
