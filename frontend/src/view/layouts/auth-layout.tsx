11a4da212dc7485bd1370f9557df734372f2ef18
Davi Silva <davisilvaphoto@gmail.com>
2025-06-29 15:26:44 -0300

    auth layout

--- frontend/src/view/layouts/auth-layout.tsx
+++ frontend/src/view/layouts/auth-layout.tsx
@@ -1,11 +1,31 @@
 import { Outlet } from "react-router-dom";
+import illustration from "../../assets/illustration.png";
+import { Logo } from "../components/logo";
 
-export const AuthLayout = () => {
-  return (
-    <div>
-      <h1>AuthLayout</h1>
+export const AuthLayout = () => (
+  <div className="flex w-full h-screen">
+    <div className="w-full h-full flex justify-center items-center flex-col gap-16 lg:w-1/2">
+      <Logo className="h-6 text-gray-500" />
 
-      <Outlet />
+      <div className="w-full max-w-[504px] px-8">
+        <Outlet />
+      </div>
     </div>
-  );
-};
+
+    <div className="w-1/2 justify-center items-center p-8 relative hidden lg:flex">
+      <img
+        src={illustration}
+        alt=""
+        className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]"
+      />
+
+      <div className="max-w-[646px] bottom-8 bg-white p-10 absolute rounded-b-[32px] text-red-500">
+        <Logo className="text-teal-900 h-8" />
+        <p className="text-gray-700 font-medium text-xl mt-6">
+          Gerencie suas finanças pessoais de uma forma simples com o fincheck, e
+          o melhor, totalmente de graça!
+        </p>
+      </div>
+    </div>
+  </div>
+);
