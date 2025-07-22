f5730cdc05aa13e7e89994607efee819190f56b1
Davi Silva <davisilvaphoto@gmail.com>
2025-07-22 09:54:13 -0300

    add custom message

--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -33,7 +33,7 @@ export class AuthGuard implements CanActivate {
 
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Missing access token');
     }
 
     try {
@@ -43,7 +43,7 @@ export class AuthGuard implements CanActivate {
 
       request['userId'] = payload.sub;
     } catch {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid access token');
     }
 
     return true;
